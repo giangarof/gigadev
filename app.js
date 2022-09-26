faf2b5267b319aa826d172453d8da56f90f9a27e
Gianmarco G <giangarof98@gmail.com>
2022-09-26 13:31:27 -0400

    new

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 app.use(express.static("style"));
 
 app.get('/', (req, res) => {
